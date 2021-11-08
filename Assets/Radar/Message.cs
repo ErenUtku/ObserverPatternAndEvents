98adacd6b73e4ad25414bfa8cccc4acba60104fc
ErenUtku <61837087+ErenUtku@users.noreply.github.com>
2021-11-08 16:46:29 +0300

    Init

---
+++ Assets/Radar/Message.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour
+{
+    public Text message;
+    void Start()
+    {
+        message=this.GetComponent<Text>();
+        message.enabled = false;
+    }
+    public void SetMessage(GameObject go)
+    {
+        message.text = "You Collected an Egg";
+        message.enabled = true;
+        Invoke("Turnoff", 5f);
+    }
+    void Turnoff()
+    {
+        message.enabled = false;
+    }
+}
